cd5dbe958d0c383082041927bc9a1a69552bf049
Hiruni Perera <hiruni.p@eyepax.com>
2022-11-10 16:25:03 +0530

    add custom image

--- dist/components/Hero.js
+++ dist/components/Hero.js
@@ -11,18 +11,23 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 const Hero = _ref => {
   let {
     title = "Always make room for beauty in your life",
-    description = "At Prauge we are passionate about making people feel good while looking their best. Attending advanced education allows us to keep up with the latest trends and provide each guest with a unique & customizable result. For your convenience, Schedule your reservation today!"
+    description = "At Prauge we are passionate about making people feel good while looking their best. Attending advanced education allows us to keep up with the latest trends and provide each guest with a unique & customizable result. For your convenience, Schedule your reservation today!",
+    image = require("../assets/about-background.png")
   } = _ref;
+  const blendImage = require("../assets/heroImage-background-blend.png");
   return /*#__PURE__*/_react.default.createElement("div", {
-    class: "hero_main"
+    className: "hero_main"
   }, /*#__PURE__*/_react.default.createElement("div", {
-    class: "hero_background"
+    className: "hero_background",
+    style: {
+      backgroundImage: ["url(".concat(image, ")"), "url(".concat(blendImage, ")")]
+    }
   }, /*#__PURE__*/_react.default.createElement("div", {
-    class: "hero_text"
+    className: "hero_text"
   }, /*#__PURE__*/_react.default.createElement("h4", {
-    class: "hero_title"
+    className: "hero_title"
   }, title), /*#__PURE__*/_react.default.createElement("p", {
-    class: "hero_subtitle"
+    className: "hero_subtitle"
   }, description))));
 };
 var _default = Hero;
--- src/lib/components/Hero.js
+++ src/lib/components/Hero.js
@@ -5,13 +5,22 @@ import "./styles/hero.css";
 const Hero = ({
   title = "Always make room for beauty in your life",
   description = "At Prauge we are passionate about making people feel good while looking their best. Attending advanced education allows us to keep up with the latest trends and provide each guest with a unique & customizable result. For your convenience, Schedule your reservation today!",
+  image = require("../assets/about-background.png"),
 }) => {
+  const blendImage = require("../assets/heroImage-background-blend.png")
   return (
-    <div class="hero_main">
-      <div class="hero_background">
-        <div class="hero_text">
-          <h4 class="hero_title">{title}</h4>
-          <p class="hero_subtitle">{description}</p>
+    <div className="hero_main">
+      <div
+        className="hero_background"
+        style={{
+          backgroundImage:
+           [ `url(${image})`, `url(${blendImage})`],
+            
+        }}
+      >
+        <div className="hero_text">
+          <h4 className="hero_title">{title}</h4>
+          <p className="hero_subtitle">{description}</p>
         </div>
       </div>
     </div>
