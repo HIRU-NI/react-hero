b13992ca115767df46682ff7dc700f8d239bec59
Hiruni Perera <hiruni.p@eyepax.com>
2022-11-10 12:57:16 +0530

    buid added text styles

--- dist/components/Hero.js
+++ dist/components/Hero.js
@@ -14,9 +14,10 @@ const Hero = () => {
     class: "hero_background"
   }, /*#__PURE__*/_react.default.createElement("div", {
     class: "hero_text"
-  }, /*#__PURE__*/_react.default.createElement("h4", null, "Always make room for beauty in your life"), /*#__PURE__*/_react.default.createElement("p", {
-    gutterBottom: true,
-    className: "hero_subtitle"
+  }, /*#__PURE__*/_react.default.createElement("h4", {
+    class: "hero_title"
+  }, "Always make room for beauty in your life"), /*#__PURE__*/_react.default.createElement("p", {
+    class: "hero_subtitle"
   }, "At Prauge we are passionate about making people feel good while looking their best. Attending advanced education allows us to keep up with the latest trends and provide each guest with a unique & customizable result. For your convenience, Schedule your reservation today!"))));
 };
 var _default = Hero;
