60fc6431422e89c15c2d73d5300f5e3baed29996
Hiruni Perera <hiruni.p@eyepax.com>
2022-11-10 09:51:51 +0530

    initial commit

---
+++ src/lib/components/Hero.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import "./styles/hero.css"
+
+const Hero = () => {
+  return (
+    <div class="hero_main">
+      <div class="hero_background">
+        <div class="hero_text">
+          <h4>Always make room for beauty in your life</h4>
+          <p gutterBottom className="hero_subtitle">
+            At Prauge we are passionate about making people feel good while
+            looking their best. Attending advanced education allows us to keep
+            up with the latest trends and provide each guest with a unique &
+            customizable result. For your convenience, Schedule your reservation
+            today!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
