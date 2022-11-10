e0214919d798a751f3da3b12857cb6ae13baadfc
Hiruni Perera <hiruni.p@eyepax.com>
2022-11-10 13:02:18 +0530

    add custom title and description

--- dist/components/Hero.js
+++ dist/components/Hero.js
@@ -4,10 +4,15 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
+require("core-js/modules/es.symbol.description.js");
 var _react = _interopRequireDefault(require("react"));
 require("./styles/hero.css");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-const Hero = () => {
+const Hero = _ref => {
+  let {
+    title = "Always make room for beauty in your life",
+    description = "At Prauge we are passionate about making people feel good while looking their best. Attending advanced education allows us to keep up with the latest trends and provide each guest with a unique & customizable result. For your convenience, Schedule your reservation today!"
+  } = _ref;
   return /*#__PURE__*/_react.default.createElement("div", {
     class: "hero_main"
   }, /*#__PURE__*/_react.default.createElement("div", {
@@ -16,9 +21,9 @@ const Hero = () => {
     class: "hero_text"
   }, /*#__PURE__*/_react.default.createElement("h4", {
     class: "hero_title"
-  }, "Always make room for beauty in your life"), /*#__PURE__*/_react.default.createElement("p", {
+  }, title), /*#__PURE__*/_react.default.createElement("p", {
     class: "hero_subtitle"
-  }, "At Prauge we are passionate about making people feel good while looking their best. Attending advanced education allows us to keep up with the latest trends and provide each guest with a unique & customizable result. For your convenience, Schedule your reservation today!"))));
+  }, description))));
 };
 var _default = Hero;
 exports.default = _default;
